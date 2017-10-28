0fea422118ab99055c6ad846ff1aefc0ac91a262
Tyler Jurecki <jurecki07@yahoo.com>
2017-10-28 17:25:48 -0400

    initial commit

---
+++ src/main/java/Jurecki07/MyFirstThymleaf/MyFirstThymleafApplication.java
@@ -0,0 +1,12 @@
+package Jurecki07.MyFirstThymleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstThymleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstThymleafApplication.class, args);
+	}
+}
---
+++ src/test/java/Jurecki07/MyFirstThymleaf/MyFirstThymleafApplicationTests.java
@@ -0,0 +1,16 @@
+package Jurecki07.MyFirstThymleaf;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyFirstThymleafApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
